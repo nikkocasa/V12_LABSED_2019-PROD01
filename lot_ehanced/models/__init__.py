d33f92bf34550907bcbfbc65c226fcc06e5b4ed5
nicolas <admin@es-natura.com>
2019-05-07 19:34:58 +0200

    addoçng sub projet E12_...

--- lot_ehanced/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import production_lot, product, product_category, ir_sequence
--- product_ehanced/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_codification, product_template, product, product_category
